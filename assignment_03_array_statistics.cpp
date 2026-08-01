94cd75453005be3e652c4be0140fcb61812d8449
self-reliantkid <syfolikumah@gmail.com>
2026-08-01 20:16:24 +0000

    feat: complete assignment03

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,84 @@
 #include <iostream>
 using namespace std;
 
+
+int sum(int arr[], int s) {
+    int total = 0;
+    
+    for (int i = 0; i < s; i++) {
+        total += arr[i];
+    }
+
+    return total;
+}
+
+
+double average(int arr[], int s) {
+    int total = sum(arr, s);
+    double avg = static_cast<double>(total) / s;
+    return avg;
+}
+
+
+int maximum(int arr[], int s) {
+    int max_value = arr[0];
+
+    for (int i = 1; i < s; i++) {
+        if (arr[i] > max_value) {
+            max_value = arr[i];
+        }
+    }
+
+    return max_value;
+}
+
+
+int minimum(int arr[], int s) {
+    int min_value = arr[0];
+
+    for (int i = 1; i < s; i++) {
+        if (arr[i] < min_value) {
+            min_value = arr[i];
+        }
+    }
+
+    return min_value;
+}
+
+
+int main() {
+    int numCount;
+    cout << "How many numbers? ";
+    cin >> numCount;
+
+    if (numCount <= 0) {
+        cout << "N must be a positive integer!";
+    }
+    
+    else {
+        int numArray[numCount];
+        int i = 1;
+
+        while (i <= numCount) {
+            int num;
+            cout << "Enter number " << i << ": ";
+            cin >> num;
+
+            numArray[i-1] = num;
+            i++;
+        }
+
+        int sumResult = sum(numArray, numCount);
+        double avgResult = average(numArray, numCount);
+        int maxResult = maximum(numArray, numCount);
+        int minResult = minimum(numArray, numCount);
+
+        cout << "\nResults:";
+        cout << "\nSum: " << sumResult;
+        cout << "\nAverage: " << avgResult;
+        cout << "\nMaximum: " << maxResult;
+        cout << "\nMinimum: " << minResult;
+    }
+
+    return 0;
+}

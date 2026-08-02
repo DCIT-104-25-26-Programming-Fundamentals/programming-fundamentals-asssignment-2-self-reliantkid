5691f43f6771733f1ab3410cb50f1c2415353ffd
self-reliantkid <syfolikumah@gmail.com>
2026-08-02 19:00:07 +0000

    feat: complete assignment01

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,34 @@
 #include <iostream>
 using namespace std;
 
+
+bool isPrime(int n) {
+    if (n < 2) {
+        return false;
+    }
+    else {
+        for (int i = 2; i * i <= n; i ++) {
+            if (n%i == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    bool result = isPrime(num);
+
+    if (result == true) {
+        cout << num << " is a prime number." << endl;
+    }
+    else {
+        cout << num << " is NOT a prime number." << endl;
+    }
+    return 0;
+}
